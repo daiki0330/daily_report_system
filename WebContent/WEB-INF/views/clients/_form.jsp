1bc7057c873691a858609d513a605264e4ec1ec4
daiki.kuwabara <work.daiki1224@gmail.com>
2020-11-20 22:10:50 +0900

    Add Report edit

--- WebContent/WEB-INF/views/clients/_form.jsp
+++ WebContent/WEB-INF/views/clients/_form.jsp
@@ -18,20 +18,20 @@
 <c:out value="${sessionScope.login_employee.name}" />
 <br /><br />
 
-<label for="company">会社名</label><br />
-<input type="text" name="company" value="${client.company}" />
+<label for="title">会社名</label><br />
+<input type="text" name="title" value="${client.company_name}" />
 <br /><br />
 
 <label for="content">商談内容</label><br />
 <textarea name="content" rows="10" cols="50">${client.content}</textarea>
 <br /><br />
 
-<label for="admin_flag">商談状況</label><br />
-<select name="admin_flag">
-    <option value="0"<c:if test="${client.admin_flag == 0}"> selected</c:if>>見積</option>
-    <option value="1"<c:if test="${client.admin_flag == 1}"> selected</c:if>>提案</option>
-    <option value="2"<c:if test="${client.admin_flag == 2}"> selected</c:if>>商談</option>
-    <option value="3"<c:if test="${client.admin_flag == 3}"> selected</c:if>>受注</option>
+<label for="status_flag">商談状況</label><br />
+<select name="status_flag">
+    <option value="0"<c:if test="${client.status_flag == 0}"> selected</c:if>>見積</option>
+    <option value="1"<c:if test="${client.status_flag == 1}"> selected</c:if>>提案</option>
+    <option value="2"<c:if test="${client.status_flag == 2}"> selected</c:if>>商談</option>
+    <option value="3"<c:if test="${client.status_flag == 3}"> selected</c:if>>受注</option>
 </select>
 <br /><br />
 
--- src/controllers/clients/ClientsShowServlet.java
+++ src/controllers/clients/ClientsShowServlet.java
@@ -1,8 +1,8 @@
 package controllers.clients;
 
 import java.io.IOException;
-import java.sql.Date;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,18 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Client;
+import utils.DBUtil;
 
 /**
- * Servlet implementation class ClientsNewServlet
+ * Servlet implementation class ClientsShowServlet
  */
-@WebServlet("/clients/new")
-public class ClientsNewServlet extends HttpServlet {
+@WebServlet("/clients/show")
+public class ClientsShowServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ClientsNewServlet() {
+    public ClientsShowServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,13 +32,16 @@ public class ClientsNewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Client c = em.find(Client.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("client", c);
         request.setAttribute("_token", request.getSession().getId());
 
-        Client c = new Client();
-        c.setClient_date(new Date(System.currentTimeMillis()));
-        request.setAttribute("client", c);
-
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/clients/new.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/clients/show.jsp");
         rd.forward(request, response);
     }
 
